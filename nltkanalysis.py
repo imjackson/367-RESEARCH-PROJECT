419f9b00556ebd6316996fe7ea8a8266e1b74de1
dbpl566 <54877378+dbpl566@users.noreply.github.com>
2021-12-02 13:49:07 -0500

    nltk analysis programs + output csv files

---
+++ nltkanalysis.py
@@ -0,0 +1,51 @@
+# CISC367-010
+# Group Members: Brianna Hunt, Debra Lymon, Jackson Pack
+# This program takes in a csv file containing threads from Slack communities
+# The data is placed in a DataFrame using pandas for easier access/organization
+# The data is re-grouped by thread number in the DataFrame
+# Then, for each thread, NLTK Vader Sentiment Analyzer is executed to determine the positivity scores of each thread
+# These results are output into a csv file pairing each thread number to its positivity scores
+
+
+import nltk
+import pandas as pd
+import csv
+from itertools import zip_longest
+import numpy as np
+from nltk.sentiment import SentimentIntensityAnalyzer
+#nltk.download()
+
+filepath = input("Enter csv filename: ") #include csv extension
+og_data = pd.read_csv(filepath) #reading original csv data into a DataFrame
+data = (og_data.groupby('thread')).first() #indexed by thread number
+scores=[]
+thread_index = []
+
+# Running NLTK Vader Sentiment Analyzer on each thread
+# returns
+def nltk_thread(data):
+    for ind in data.index: #data is indexed by thread
+        sia = SentimentIntensityAnalyzer()
+        output = sia.polarity_scores(data.message[ind])
+        #print(output)
+        scores.append(output) #store score dictionaries in list
+        thread_index.append(ind) #store thread num in list
+
+
+def main():
+    nltk_thread(data)
+
+    #write to csv file
+    thread = thread_index
+    positivity_score = scores
+    csv_data = [thread, positivity_score]
+    export_data=zip_longest(*csv_data, fillvalue = '')
+    write_filepath = input("Enter file name you would like to save as: ") #Enter with .csv extension included
+    with open(write_filepath, 'w', encoding="ISO-8859-1", newline='') as file:
+        write = csv.writer(file)
+        write.writerow(("Thread", "Positivity Scores"))
+        write.writerows(export_data)
+
+
+if __name__ == "__main__":
+    main()
---
+++ readme.md
@@ -0,0 +1,11 @@
+### CISC367-010
+###Group Members: Brianna Hunt, Debra Lymon, Jackson Pack
+## concat_group_messages.py
+#### This program (provided by Dr.Pollock and modified by Debra Lymon) allows users to input a disentangled xml chat file and create a csv (which groups conversations by conversation ids and concatenates consecutive utterances from same speaker in a conversation (adds semicolon between concatenated utterances). 
+#### The output csv files of this program are in the 'original_xml_to_csv' folder.
+## nltkanalysis.py
+#### This program allows users to input a csv file of Slack chat data to perform sentiment analysis on each thread using NLTK Vader.
+##### Users can input their chosen csv file name (we used the csv files from the 'original_xml_to_csv' folder).
+##### For each thread, NLTK Vader Sentiment Analyzer is executed to determine the positivity scores of each thread.  
+##### These results are output into a csv file pairing each thread number to its positivity scores.
+##### The output csv files of this program are in the 'nltk_output_csv' folder.
